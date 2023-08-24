6172f71545528ba38001e864885bc43ffa8e03c2
RawanSayedd <rawansayed143@gmail.com>
2023-08-24 22:57:48 +0300

    navbar mod

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const menuIcon = document.querySelector(".menu-btn");
 const menuList = document.querySelector(".menu-list");
 const cancelIcon = document.querySelector(".cancel-btn");
-
 menuIcon.onclick = () => {
   menuList.classList.add("active");
   menuIcon.classList.add("hide");
@@ -9,4 +8,11 @@ menuIcon.onclick = () => {
 cancelIcon.onclick = () => {
   menuList.classList.remove("active");
   menuIcon.classList.remove("hide");
+};
+const navBar = document.querySelector(".navbar");
+
+window.onscroll = () => {
+  this.scrollY > 20
+    ? navBar.classList.add("sticky")
+    : navBar.classList.remove("sticky");
 };
